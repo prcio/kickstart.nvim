160159fb942567b2076ddc8b1fb0b0ec2effc73b
icrep <xspikerxgarcia@gmail.com>
2024-11-01 21:12:39 -0400

    Cleaning things up.

--- init.lua
+++ init.lua
@@ -1,78 +1,67 @@
+-- Disable Netrw to avoid conflicts with file explorer plugins
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
-vim.opt.termguicolors = true
+vim.opt.termguicolors = true  -- Enable true color support
 
+-- Leader key settings
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- Set Nerd Font availability
 vim.g.have_nerd_font = true
 
-vim.opt.number = true
-
-vim.opt.mouse = 'a'
-
-vim.opt.showmode = false
-
+-- UI and Editor Behavior
+vim.opt.number = true                -- Show line numbers
+vim.opt.mouse = 'a'                  -- Enable mouse mode
+vim.opt.showmode = false             -- Don't display mode (e.g., -- INSERT --) in command line
 vim.schedule(function()
-  vim.opt.clipboard = 'unnamedplus'
+  vim.opt.clipboard = 'unnamedplus'  -- Sync clipboard with OS
 end)
-
-vim.opt.breakindent = true
-
-vim.opt.undofile = true
-
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
-vim.opt.signcolumn = 'yes'
-
-vim.opt.updatetime = 250
-
-vim.opt.timeoutlen = 300
-
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-
-vim.opt.list = true
+vim.opt.breakindent = true           -- Enable break indent
+vim.opt.undofile = true              -- Save undo history across sessions
+vim.opt.ignorecase = true            -- Case insensitive search
+vim.opt.smartcase = true             -- But be case sensitive if query contains capital letters
+vim.opt.signcolumn = 'yes'           -- Always show sign column
+vim.opt.updatetime = 250             -- Faster update time for better UI feedback
+vim.opt.timeoutlen = 300             -- Shorter timeout for mapped sequences
+vim.opt.splitright = true            -- Open new vertical splits to the right
+vim.opt.splitbelow = true            -- Open new horizontal splits below
+vim.opt.list = true                  -- Show special characters like tabs
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.inccommand = 'split'         -- Live preview for search/replace
+vim.opt.cursorline = true            -- Highlight the current line
+vim.opt.scrolloff = 10               -- Keep cursor away from screen edge
 
-vim.opt.inccommand = 'split'
-
-vim.opt.cursorline = true
-
-vim.opt.scrolloff = 10
-
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
-
+-- Keymaps for navigation and functionality
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')  -- Clear search highlights
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+-- Arduino Keymaps
 local function open_split_term(cmd)
-  -- Open a horizontal split and create a terminal
-  vim.cmd('split') -- Open a new horizontal split
-  vim.cmd('resize 15') -- Resize the split window to be 15 lines high
-  local buf = vim.api.nvim_create_buf(false, true) -- Create a new buffer
-  vim.api.nvim_set_current_buf(buf) -- Set the split to use the new buffer
-  vim.fn.termopen(cmd) -- Run the terminal command in the split window
-  vim.cmd('startinsert') -- Enter insert mode to interact with the terminal
+  vim.cmd('split')  -- Open a horizontal split
+  vim.cmd('resize 15')  -- Resize to 15 lines high
+  local buf = vim.api.nvim_create_buf(false, true)  -- Create a new buffer
+  vim.api.nvim_set_current_buf(buf)  -- Set buffer to split window
+  vim.fn.termopen(cmd)  -- Run command in terminal
+  vim.cmd('startinsert')  -- Enter insert mode
 end
 
--- Compile the current sketch in a split terminal
 vim.keymap.set('n', '<leader>ac', function()
   open_split_term('arduino-cli compile --fqbn arduino:avr:uno "' .. vim.fn.expand('%:p') .. '"')
 end, { desc = '[A]rduino [C]ompile' })
 
--- Upload the current sketch in a split terminal
 vim.keymap.set('n', '<leader>au', function()
   open_split_term('arduino-cli upload -p /dev/ttyACM* --fqbn arduino:avr:uno "' .. vim.fn.expand('%:p:h') .. '"')
 end, { desc = '[A]rduino [U]pload' })
 
-
+-- Window navigation
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Highlight text on yank
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
@@ -81,6 +70,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Plugin Manager - Lazy.nvim
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.uv.fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
@@ -91,32 +81,13 @@ if not vim.uv.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup {
-  'tpope/vim-sleuth',
-  'nvim-lua/plenary.nvim',
+-- Load default plugins and custom plugins from plugins.lua
+require('lazy').setup({
+  -- Default Plugins
+  'tpope/vim-sleuth',  -- Auto-detect indentation style
+  'nvim-lua/plenary.nvim',  -- Utility library for various plugins
   {
-    'prcio/startup.nvim',
-    requires = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
-    config = function()
-      require('startup').setup { theme = 'evil' }
-    end,
-  },
-  { 'ThePrimeagen/harpoon', branch = 'harpoon2', dependencies = { 'nvim-lua/plenary.nvim' } },
-  'nvim-tree/nvim-web-devicons',
-  'echasnovski/mini.base16',
-  {
-    'nvim-tree/nvim-tree.lua',
-    version = '*',
-    lazy = false,
-    dependencies = {
-      'nvim-tree/nvim-web-devicons',
-    },
-    config = function()
-      require('nvim-tree').setup {}
-    end,
-  },
-  {
-    'lewis6991/gitsigns.nvim',
+    'lewis6991/gitsigns.nvim',  -- Git signs in gutter
     opts = {
       signs = {
         add = { text = '+' },
@@ -128,11 +99,10 @@ require('lazy').setup {
     },
   },
   {
-    'folke/which-key.nvim',
+    'folke/which-key.nvim',  -- Keybinding pop-up
     event = 'VimEnter',
     config = function()
       require('which-key').setup()
-
       require('which-key').add {
         { '<leader>c', group = '[C]ode' },
         { '<leader>d', group = '[D]ocument' },
@@ -145,117 +115,65 @@ require('lazy').setup {
     end,
   },
   {
-    'nvim-telescope/telescope.nvim',
+    'nvim-telescope/telescope.nvim',  -- Fuzzy Finder
     event = 'VimEnter',
     branch = '0.1.x',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      {
-        'nvim-telescope/telescope-fzf-native.nvim',
-        build = 'make',
-        cond = function()
-          return vim.fn.executable 'make' == 1
-        end,
-      },
-      { 'nvim-telescope/telescope-ui-select.nvim' },
-      { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
-    },
+    dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
       require('telescope').setup {
+        defaults = { mappings = { i = { ['<c-x>'] = 'close' } } },
         extensions = {
-          ['ui-select'] = {
-            require('telescope.themes').get_dropdown(),
-          },
+          ['ui-select'] = { require('telescope.themes').get_dropdown() },
         },
       }
-
       pcall(require('telescope').load_extension, 'fzf')
       pcall(require('telescope').load_extension, 'ui-select')
-
-      local builtin = require 'telescope.builtin'
-      vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-      vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-      vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-      vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-      vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-      vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-      vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files' })
-      vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-
-      vim.keymap.set('n', '<leader>/', function()
-        builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-          winblend = 10,
-          previewer = false,
-        })
-      end, { desc = '[/] Fuzzily search in current buffer' })
-
-      vim.keymap.set('n', '<leader>s/', function()
-        builtin.live_grep {
-          grep_open_files = true,
-          prompt_title = 'Live Grep in Open Files',
-        }
-      end, { desc = '[S]earch [/] in Open Files' })
-
-      vim.keymap.set('n', '<leader>sn', function()
-        builtin.find_files { cwd = vim.fn.stdpath 'config' }
-      end, { desc = '[S]earch [N]eovim files' })
     end,
   },
   {
-    'folke/tokyonight.nvim',
+    'folke/tokyonight.nvim',  -- Colorscheme
     priority = 1000,
     init = function()
       vim.cmd.colorscheme 'tokyonight-night'
       vim.cmd.hi 'Comment gui=none'
     end,
   },
-  'folke/todo-comments.nvim',
-  'nvim-treesitter/nvim-treesitter',
+  'folke/todo-comments.nvim',  -- Highlights TODO comments
   {
-    'neovim/nvim-lspconfig',
+    'nvim-treesitter/nvim-treesitter',  -- Enhanced syntax highlighting
+    opts = {
+      ensure_installed = { 'lua', 'python', 'javascript' },
+      highlight = { enable = true },
+      indent = { enable = true },
+    },
+  },
+  {
+    'neovim/nvim-lspconfig',  -- LSP configurations
     dependencies = {
       'williamboman/mason.nvim',
       'williamboman/mason-lspconfig.nvim',
-      'WhoIsSethDaniel/mason-tool-installer.nvim',
       'hrsh7th/cmp-nvim-lsp',
-      'j-hui/fidget.nvim',
+      'j-hui/fidget.nvim',  -- LSP status notifications
     },
     config = function()
       require('mason').setup()
       require('mason-lspconfig').setup {
-        ensure_installed = { 'arduino_language_server', 'clangd' },
+        ensure_installed = { 'lua_ls', 'clangd' },
       }
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
-
-      local lspconfig = require 'lspconfig'
-      lspconfig.arduino_language_server.setup {
-        cmd = {
-          'arduino-language-server',
-          '-cli-config',
-          '~/.arduino15/arduino-cli.yaml',
-          '-fqbn',
-          'arduino:avr:uno',
-          '-cli',
-          'arduino-cli',
-          '-clangd',
-          'clangd',
-        },
-        capabilities = capabilities,
-      }
+      require('lspconfig').lua_ls.setup {}
     end,
   },
-  'hrsh7th/nvim-cmp',
-  'L3MON4D3/LuaSnip',
-}
+  'hrsh7th/nvim-cmp',  -- Autocompletion plugin
+  'L3MON4D3/LuaSnip',  -- Snippet engine
 
+  -- Custom plugins from plugins.lua
+  { import = 'custom.plugins' },
+})
+
+-- Terminal background restore for Kitty
 vim.cmd [[
 augroup RestoreBackground
   autocmd!
   autocmd VimLeave * silent !kitty @ set-colors background=#1e1e2e
 augroup END
 ]]
-
--- vim: ts=2 sts=2 sw=2 et
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,29 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'prcio/startup.nvim',  -- Startup screen with custom theme
+    requires = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
+    config = function()
+      require('startup').setup { theme = 'evil' }
+    end,
+  },
+  {
+    'ThePrimeagen/harpoon',  -- Quick file navigation
+    branch = 'harpoon2',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+  },
+  'nvim-tree/nvim-web-devicons',  -- Adds icons for file types
+  'echasnovski/mini.base16',  -- Color scheme based on base16
+  {
+    'nvim-tree/nvim-tree.lua',  -- File explorer
+    version = '*',
+    lazy = false,
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('nvim-tree').setup {}
+    end,
+  },
+}
+
